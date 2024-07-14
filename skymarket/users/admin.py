434890bd2e4d4ce1c163fdb35a17f849ef242446
ssher <ssher250110@yandex.ru>
2024-07-14 19:48:50 +0300

    added view model User in admin panel

--- skymarket/users/admin.py
+++ skymarket/users/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from users.models import User
-# TODO Aдмика для пользователя - как реализовать ее можно подсмотреть в документаци django
-# TODO Обычно её всегда оформляют, но в текущей задачи делать её не обязательно
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "email", "first_name", "last_name", "phone", "role", "is_active", "last_login"]
+    ordering = ["email"]
