7b24d67ec4ed8c9e64602f18c86847018367fad5
ssher <ssher250110@yandex.ru>
2024-07-15 03:44:55 +0300

    editing admin panel model User

--- skymarket/users/admin.py
+++ skymarket/users/admin.py
@@ -7,4 +7,6 @@ from users.models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ["id", "email", "first_name", "last_name", "phone", "role", "is_active", "last_login"]
+    search_fields = ["email", "first_name", "last_name", "last_login"]
+    list_filter = ["role", "is_active"]
     ordering = ["email"]
