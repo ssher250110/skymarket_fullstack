5754dde5a1e1182b04eab6be5400ea3ef06bb1eb
ssher <ssher250110@yandex.ru>
2024-07-15 03:49:14 +0300

    editing admin panel models Ad and Comment

--- skymarket/ads/admin.py
+++ skymarket/ads/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 
 from ads.models import Ad, Comment
 
-# TODO здесь можно подкючить ваши модели к стандартной джанго-админке
 
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "description", "price", "created_at", "author"]
+    search_fields = ["title"]
+    list_filter = ["title", "author"]
+    ordering = ["title", "price"]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["id", "text", "author", "ad", "created_at"]
+    list_filter = ["author"]
+    ordering = ["text", "ad"]
