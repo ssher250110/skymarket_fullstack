7c43bfa447ac731cd165891c9e2ee7bc38c1cd99
ssher <ssher250110@yandex.ru>
2024-07-16 12:40:46 +0300

    editing view list_display comments

--- skymarket/ads/admin.py
+++ skymarket/ads/admin.py
@@ -13,6 +13,6 @@ class AdAdmin(admin.ModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ["id", "text", "author", "ad", "created_at"]
+    list_display = ["id", "text", "ad", "created_at", "author"]
     list_filter = ["author"]
     ordering = ["text", "ad"]
