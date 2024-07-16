37280926acd6fec309e4f02b33bc80b1a76ad4c0
ssher <ssher250110@yandex.ru>
2024-07-17 01:05:16 +0300

    added permission IsAuthor

--- skymarket/ads/permissions.py
+++ skymarket/ads/permissions.py
@@ -1,10 +1,10 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsOwner(BasePermission):
+class IsAuthor(BasePermission):
     """Проверка прав доступа владельца к контроллеру объекта"""
 
     def has_object_permission(self, request, view, obj):
-        if obj.owner == request.user:
+        if obj.author == request.user:
             return True
         return False
